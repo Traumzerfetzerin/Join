99201ae0ca75f0953e3d98b43f9cb96ea1d9d05b
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2024-10-30 18:34:51 +0100

    clean code prio buttons

--- script/addTask.js
+++ script/addTask.js
@@ -29,31 +29,48 @@ function setPrio(prio, event) {
     let priorityButton = event.target.id;
 
     if (event.target.id == "low") {
-        document.getElementById(priorityButton).classList.add('lowWhite');
-        document.getElementById(`${priorityButton}Svg`).src = "../Assets/addTask/Prio_baja_white.svg";
-        document.getElementById('medium').classList.remove('mediumWhite');
-        document.getElementById('urgent').classList.remove('urgentWhite');
-        document.getElementById(`mediumSvg`).src = "../Assets/addTask/Prio media.svg";
-        document.getElementById(`urgentSvg`).src = "../Assets/addTask/Prio alta.svg";
+        lowPrioButton(priorityButton)
 
     } if (event.target.id == "medium") {
-        document.getElementById(priorityButton).classList.add('mediumWhite');
-        document.getElementById(`${priorityButton}Svg`).src = "../Assets/addTask/Prio media white.svg";
-        document.getElementById('low').classList.remove('lowWhite');
-        document.getElementById('urgent').classList.remove('urgentWhite');
-        document.getElementById(`lowSvg`).src = "../Assets/addTask/Prio baja.svg";
-        document.getElementById(`urgentSvg`).src = "../Assets/addTask/Prio alta.svg";
+        mediumPrioButton(priorityButton)
 
     } if (event.target.id == "urgent") {
-        document.getElementById(priorityButton).classList.add('urgentWhite');
-        document.getElementById(`${priorityButton}Svg`).src = "../Assets/addTask/Prio_alta_white.svg";
-        document.getElementById('low').classList.remove('lowWhite');
-        document.getElementById('medium').classList.remove('mediumWhite');
-        document.getElementById(`lowSvg`).src = "../Assets/addTask/Prio baja.svg";
-        document.getElementById(`mediumSvg`).src = "../Assets/addTask/Prio media.svg";
+        urgentPrioButton(priorityButton)
     }
 }
 
+
+// PRIO BUTTON LOW
+function lowPrioButton(priorityButton) {
+    document.getElementById(priorityButton).classList.add('lowWhite');
+    document.getElementById(`${priorityButton}Svg`).src = "../Assets/addTask/Prio_baja_white.svg";
+    document.getElementById('medium').classList.remove('mediumWhite');
+    document.getElementById('urgent').classList.remove('urgentWhite');
+    document.getElementById(`mediumSvg`).src = "../Assets/addTask/Prio media.svg";
+    document.getElementById(`urgentSvg`).src = "../Assets/addTask/Prio alta.svg";
+}
+
+
+// PRIO BUTTON MEDIUM
+function mediumPrioButton(priorityButton) {
+    document.getElementById(priorityButton).classList.add('mediumWhite');
+    document.getElementById(`${priorityButton}Svg`).src = "../Assets/addTask/Prio media white.svg";
+    document.getElementById('low').classList.remove('lowWhite');
+    document.getElementById('urgent').classList.remove('urgentWhite');
+    document.getElementById(`lowSvg`).src = "../Assets/addTask/Prio baja.svg";
+    document.getElementById(`urgentSvg`).src = "../Assets/addTask/Prio alta.svg";
+}
+
+
+// PRIO BUTTON URGENT
+function urgentPrioButton(priorityButton) {
+    document.getElementById(priorityButton).classList.add('urgentWhite');
+    document.getElementById(`${priorityButton}Svg`).src = "../Assets/addTask/Prio_alta_white.svg";
+    document.getElementById('low').classList.remove('lowWhite');
+    document.getElementById('medium').classList.remove('mediumWhite');
+    document.getElementById(`lowSvg`).src = "../Assets/addTask/Prio baja.svg";
+    document.getElementById(`mediumSvg`).src = "../Assets/addTask/Prio media.svg";
+}
 
 let taskCategory = ""; // Variable zum Speichern der ausgewählten kategorie
 
@@ -66,6 +83,7 @@ let taskCategory = ""; // Variable zum Speichern der ausgewählten kategorie
  * 
  * @param {Event} event - The click event to prevent the default form submission.
  */
+
 
 async function createTasks(event) {
     // Prevent the form from reloading the page
