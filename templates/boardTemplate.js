22131f6a8aa8cd912a501082b078ec4b74bd7dae
HergenEngelhardt <168177848+HergenEngelhardt@users.noreply.github.com>
2024-10-30 18:27:58 +0100

    added contact to the task

--- templates/boardTemplate.js
+++ templates/boardTemplate.js
@@ -1,11 +1,9 @@
 /** Template for each task */
-function getTaskBoardTemplate(category, task, taskId) {
-    return `
-        <div class="task" id="${taskId}" draggable="true" ondragstart="drag(event)">
-            <h3>${category}</h3>
-            <h4>${task.title}</h4>
-            <p>${task.description}</p>
-        </div>
-    `;
+function getTaskBoardTemplate(category, task, taskId, contactList) {
+     return `
+     <div class="task" id="${taskId}" draggable="true" ondragstart="drag(event)"> 
+        <h3>${category}</h3> 
+        <h4>${task.title}</h4> 
+        <p>${task.description}</p> 
+        <ul>${contactList}</ul> </div> `;
 }
-
