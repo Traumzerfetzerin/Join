8d15904944e7ab2e84cd1359dd0ba4ccb947ea0c
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2024-11-05 18:42:56 +0100

    delete cursor pointer board

--- templates/boardTemplate.js
+++ templates/boardTemplate.js
@@ -1,7 +1,7 @@
 /** Template for each task */
 function getTaskBoardTemplate(category, task, taskId, contactList) {
      return `
-     <div class="task" id="${taskId}" draggable="true" ondragstart="drag(event)"> 
+     <div class="task cursorPointer" id="${taskId}" draggable="true" ondragstart="drag(event)"> 
         <h3>${category}</h3> 
         <h4>${task.title}</h4> 
         <p>${task.description}</p> 
