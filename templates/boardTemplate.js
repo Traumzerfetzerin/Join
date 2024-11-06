8d29885f2f0c619d5dd51b6214f2fb42bf696201
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2024-11-06 18:01:43 +0100

    corrected board

--- templates/boardTemplate.js
+++ templates/boardTemplate.js
@@ -1,6 +1,6 @@
 /** Template for each task */
 function getTaskBoardTemplate(category, task, taskId, contactList) {
-     return `
+        return `
      <div class="task cursorPointer" id="${taskId}" draggable="true" ondragstart="drag(event)"> 
         <h3>${category}</h3> 
         <h4>${task.title}</h4> 
